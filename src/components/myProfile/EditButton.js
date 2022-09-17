6e32835865d2a78e2cd70203652a0cabe22abf8e
Harsh Kumar <harshgaya42@gmail.com>
2022-09-17 16:40:23 +0530

    commited

--- src/components/myProfile/EditButton.js
+++ src/components/myProfile/EditButton.js
@@ -5,138 +5,138 @@ import cover1 from "./../../assets/images/cover1.png"
 import cover2 from "./../../assets/images/cover2.png"
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      background: "rgb(21,15,15,0.99) ",
-      borderRadius: "20px",
-      border: "1px solid rgb(255,255,255,0.1)",
-      width: "70%"
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: "rgb(21,15,15,0.99) ",
+    borderRadius: "20px",
+    border: "1px solid rgb(255,255,255,0.1)",
+    width: "70%"
 
-    },
-    overlay: {
-        backgroundColor: 'rgb(41, 40,40,0.8)'
-      },
-  };
-  Modal.setAppElement('#root');
+  },
+  overlay: {
+    backgroundColor: 'rgb(41, 40,40,0.8)'
+  },
+};
+Modal.setAppElement('#root');
 
 function EditButtonimage() {
-    let subtitle;
-    let rowStyle;
-    let cancelButton;
-    let lineStyle;
-    let imgStyle;
-    let insideRowStyle;
-    let submitButton;
-    const [modalIsOpen, setIsOpen] = React.useState(false);
-  
-    function openModal() {
-      setIsOpen(true);
+  let subtitle;
+  let rowStyle;
+  let cancelButton;
+  let lineStyle;
+  let imgStyle;
+  let insideRowStyle;
+  let submitButton;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    }
-  
-    function afterOpenModal() {
-      // references are now sync'd and can be accessed.
-      subtitle.style.color = '#fff';
-      subtitle.style.fontSize = '30px';
-      rowStyle.style.overflow = "hidden";
-      rowStyle.style.fontFamily = 'Poppins';
-      cancelButton.style.color = "white";
-      cancelButton.style.marginLeft = "130px";
-      cancelButton.style.cursor = "pointer";
-      lineStyle.style.color = "#8E8B8B";
-      imgStyle.style.textAlign = "center";
-      insideRowStyle.style.paddingTop = "45px";
-      submitButton.style.color = "white";
-      submitButton.style.textAlign = "center";
-      submitButton.style.background = "#E82352";
-      submitButton.style.borderRadius = "10px";
-      submitButton.style.padding = "9px 18px";
-      submitButton.style.marginTop = "35px ";
-      submitButton.style.maxWidth = "320px";
-      submitButton.style.marginLeft = "auto";
-      submitButton.style.marginRight = "auto";
-    }
-  
-    function closeModal() {
-      setIsOpen(false);
-    }
-    return ( 
-        <div className="editButtonImage">
-            <button  onClick={openModal} className="logoButton"><img src={button} alt="editButton" /></button>           
-            <Modal
-            
+  function openModal() {
+    setIsOpen(true);
+
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#fff';
+    subtitle.style.fontSize = '30px';
+    rowStyle.style.overflow = "hidden";
+    rowStyle.style.fontFamily = 'Poppins';
+    cancelButton.style.color = "white";
+    cancelButton.style.marginLeft = "130px";
+    cancelButton.style.cursor = "pointer";
+    lineStyle.style.color = "#8E8B8B";
+    imgStyle.style.textAlign = "center";
+    insideRowStyle.style.paddingTop = "45px";
+    submitButton.style.color = "white";
+    submitButton.style.textAlign = "center";
+    submitButton.style.background = "#E82352";
+    submitButton.style.borderRadius = "10px";
+    submitButton.style.padding = "9px 18px";
+    submitButton.style.marginTop = "35px ";
+    submitButton.style.maxWidth = "320px";
+    submitButton.style.marginLeft = "auto";
+    submitButton.style.marginRight = "auto";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <div className="editButtonImage">
+      <button onClick={openModal} className="logoButton"><img src={button} alt="editButton" /></button>
+      <Modal
+
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Model for Cover Image"
 
-        
+
       >
         <div >
-        <div className="row" ref={(_rowStyle) => (rowStyle = _rowStyle)}>
+          <div className="row" ref={(_rowStyle) => (rowStyle = _rowStyle)}>
             <div className="col-10">
-            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Select your cover image</h2>
+              <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Select your cover image</h2>
             </div>
             <div className="col-2">
-            <a ref={(_cancelButton) => (cancelButton = _cancelButton)} onClick={closeModal}>Cancel</a>
+              <a ref={(_cancelButton) => (cancelButton = _cancelButton)} onClick={closeModal}>Cancel</a>
 
             </div>
-            
-        </div>
-        <hr ref={(_lineStyle) => (lineStyle = _lineStyle)} />
-        <div className="row">
+
+          </div>
+          <hr ref={(_lineStyle) => (lineStyle = _lineStyle)} />
+          <div className="row">
             <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-                <a  href=""><img  src={cover1} alt="" /></a>
+              <a href=""><img src={cover1} alt="" image={cover1} /></a>
             </div>
             <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a href=""><img src={cover2} alt="" /></a>
-                
-            </div>
-            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a  href=""><img src={cover1} alt="" /></a>
+              <a href=""><img src={cover2} alt="" /></a>
 
             </div>
             <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a  href=""><img src={cover2} alt="" /></a>
-            </div>
-            
-        </div>
-        <div className="row" ref={(_insideRowStyle) => (insideRowStyle = _insideRowStyle)}>
-            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-                <a  href=""><img  src={cover1} alt="" /></a>
-            </div>
-            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a href=""><img src={cover2} alt="" /></a>
-               
-            </div>
-            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a  href=""><img src={cover1} alt="" /></a>
+              <a href=""><img src={cover1} alt="" /></a>
 
             </div>
             <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
-            <a  href=""><img src={cover2} alt="" /></a>
+              <a href=""><img src={cover2} alt="" /></a>
             </div>
-            
-        </div>
-        <div ref={(_submitButton) => (submitButton = _submitButton)}>
-        <a >Submit</a>
 
+          </div>
+          <div className="row" ref={(_insideRowStyle) => (insideRowStyle = _insideRowStyle)}>
+            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
+              <a href=""><img src={cover1} alt="" /></a>
+            </div>
+            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
+              <a href=""><img src={cover2} alt="" /></a>
+
+            </div>
+            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
+              <a href=""><img src={cover1} alt="" /></a>
+
+            </div>
+            <div className="col-3" ref={(_imgStyle) => (imgStyle = _imgStyle)}>
+              <a href=""><img src={cover2} alt="" /></a>
+            </div>
+
+          </div>
+          <div ref={(_submitButton) => (submitButton = _submitButton)}>
+            <a >Submit</a>
+
+          </div>
         </div>
-        </div>
-       
-    
+
+
       </Modal>
-    
-        </div>
 
-        
-     );
+    </div>
+
+
+  );
 }
- 
+
 export default EditButtonimage;
--- src/components/myProfile/MyProfile.js
+++ src/components/myProfile/MyProfile.js
@@ -5,18 +5,167 @@ import ProfilePic from "./ProfilePic";
 import Button from "./button";
 import GamesPlayed from "./GamesPlayed";
 import PostView from "./PostView";
+import ReactDOM from 'react-dom';
 
 import postIcon from "./../../assets/images/post-icon.png"
 import byteIcon from "./../../assets/images/bytes-icon.png"
-import profile_img from "./../../assets/images/profile_img.png"
-const MyProfile = () => {
+import profile_img from "./../../assets/images/profile_img.png";
+import { useState } from 'react';
+import button from "./../../assets/images/EditButton.png"
+import cover1 from "./../../assets/images/cover1.png"
+import cover2 from "./../../assets/images/cover2.png";
+import crossIcon from "./../../assets/images/cross.png";
+import gameImage1 from "./../../assets/h/gameimage1.jpg";
+import gameImage2 from "./../../assets/h/gameimage2.jpg";
+import gameImage3 from "./../../assets/h/gameimage3.jpg";
+import gameImage4 from "./../../assets/h/gameimage4.jpg";
+import gameImage5 from "./../../assets/h/gameimage5.webp";
+import gameImage6 from "./../../assets/h/gameimage6.jpg";
+import gameImage7 from "./../../assets/h/gameimage7.jpg";
+import gameImage8 from "./../../assets/h/gameimage8.jpg";
+import gameImage9 from "./../../assets/h/gameimage9.jpg";
+
+
+
+
+const MyProfile = (props) => {
     // const url = "./../asse//ts/images/cover.png"
+    const [backdrop, setBackdrop] = useState(false);
+    const [coverImage, setCoverImage] = useState(cover_img);
+    const [gridSelectedImage, setGridSelectedImage] = useState(gameImage1);
+    const [imageClassName, setImageClassName] = useState('grid-single-image');
+
+    const list_of_cover_images = [
+        {
+            id: 'gameImage1',
+            title: 'gameImage1',
+            src: gameImage1
+        },
+        {
+            id: 'gameImage2',
+            title: 'gameImage2',
+            src: gameImage2
+        },
+        {
+            id: 'gameImage3',
+            title: 'gameImage3',
+            src: gameImage3
+        },
+        {
+            id: 'gameImage4',
+            title: 'gameImage4',
+            src: gameImage4
+        },
+        {
+            id: 'gameImage5',
+            title: 'gameImage5',
+            src: gameImage5
+        }
+        ,
+        {
+            id: 'gameImage6',
+            title: 'gameImage6',
+            src: gameImage6
+        },
+        {
+            id: 'gameImage7',
+            title: 'gameImage7',
+            src: gameImage7
+        },
+        {
+            id: 'gameImage8',
+            title: 'gameImage8',
+            src: gameImage8
+        }
+        ,
+        {
+            id: 'gameImage9',
+            title: 'gameImage9',
+            src: gameImage9
+        }
+    ]
+
+
+    const editBackgroundImageHandler = () => {
+        setBackdrop(true);
+    }
+    const removeBackdrop = () => {
+        setBackdrop(false);
+    }
+
+    const coverImageChangeHandler = (event) => {
+
+
+        setCoverImage(gridSelectedImage);
+        setBackdrop(false);
+
+    }
+    const changeGridSelectedImage = (event) => {
+        console.log(event.target);
+        // event.target.style.border = '5px solid red';
+        setImageClassName('grid-single-selected-image');
+        setGridSelectedImage(event.target.src);
+
+    }
+
+
+
+
     return (
         <div className="myProfile">
             <div className="container">
                 <div className="card background">
-                    <img className="coverImage" src={cover_img} alt="Profile Cover" />
-                    <EditbuttonImage />
+                    <img className="coverImage" src={coverImage} alt="Profile Cover" />
+                    {/* <EditbuttonImage  /> */}
+                    <button onClick={editBackgroundImageHandler} className="logoButton"><img src={button} alt="editButton" /></button>
+
+                    {backdrop && <div>
+
+                        {ReactDOM.createPortal(
+                            <div className="backdrop-for-profile" onClick={removeBackdrop}>
+
+                            </div>, document.getElementById('profile-backdrop')
+                        )}
+
+                        <div className="choose-banner-pop-up">
+                            <img src={crossIcon} alt="cross-button" onClick={removeBackdrop} />
+
+                            <div className="banner-lists">
+                                <h3>Choose banner</h3>
+                                <div className="banner-images">
+                                    <div className="selected-banner-image">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ?</p>
+                                        <img src={gridSelectedImage} alt="gameimage" />
+
+                                    </div>
+                                    <div className="grid-images">
+
+                                        {
+                                            list_of_cover_images.map((coverImage) => (
+                                                <img src={coverImage.src} alt={coverImage.title} key={coverImage.id} onClick={changeGridSelectedImage} className='grid-single-image' />
+                                            ))
+                                        }
+
+
+
+                                    </div>
+
+
+
+                                </div>
+                                <div className="button-done">
+                                    <button onClick={coverImageChangeHandler}>Done</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>}
+
+
+
                     <div className="row alignContent">
                         <div className="col-1">
                             <ProfilePic imgurl={profile_img} />
@@ -43,11 +192,11 @@ const MyProfile = () => {
                             <Button />
                         </div>
                         <div className="gamesPlayedMain">
-                        <GamesPlayed />
+                            <GamesPlayed />
                         </div>
                         <div className="postview">
-                            <PostView  />
-                        </div>       
+                            <PostView />
+                        </div>
 
                     </div>
 
